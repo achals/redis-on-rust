c2b6cd0d72dbaa4940c62bd7af4b16812b44b921
Achal Shah <achal@tecton.ai>
2024-10-02 23:32:07 -0700

    feat: Actually read requests
    Signed-off-by: Achal Shah <achal@tecton.ai>

--- src/redis_server.rs
+++ src/redis_server.rs
@@ -1,5 +1,5 @@
-use std::io::{Error};
-use std::net::{TcpListener};
+use std::io::{BufRead, BufReader, BufWriter, Error, Write};
+use std::net::{TcpListener, TcpStream};
 
 pub struct RedisServer {
     tcp_listener: TcpListener,
@@ -8,23 +8,47 @@ pub struct RedisServer {
 impl RedisServer {
     pub fn new(port: u16) -> RedisServer {
         RedisServer {
-            tcp_listener: TcpListener::bind( format!("127.0.0.1:{}", port.to_string())).unwrap(),
+            tcp_listener: TcpListener::bind(format!("127.0.0.1:{}", port.to_string())).unwrap(),
         }
     }
 
     pub fn start(&mut self) -> Result<(), Error> {
-            for stream in self.tcp_listener.incoming() {
-                match stream {
-                    Ok(stream) => {
-                        log::info!("Connection established: {:?}", stream.peer_addr()?);
+        for stream in self.tcp_listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    log::info!("Connection established: {:?}", stream.peer_addr()?);
 
-                        // handle_connection(stream);
-                    }
-                    Err(e) => {
-                        log::error!("Failed to establish connection: {:?}", e);
-                    }
+                    self.handle_connection(stream);
+                }
+                Err(e) => {
+                    log::error!("Failed to establish connection: {:?}", e);
                 }
             }
-            Ok(())
+        }
+        Ok(())
     }
+
+    fn handle_connection(&self, stream: TcpStream) {
+        let mut reader = BufReader::new(&stream);
+        let mut writer = BufWriter::new(&stream);
+
+        loop {
+            let mut buffer = String::new();
+            match reader.read_line(&mut buffer) {
+                Ok(0) => {
+                    log::info!("Connection closed");
+                    break;
+                }
+                Ok(_) => {
+                    log::info!("Received: {}", buffer);
+                    writer.write_all(buffer.as_bytes()).unwrap();
+                }
+                Err(e) => {
+                    log::error!("Failed to read from stream: {:?}", e);
+                    break;
+                }
+            }
+        }
+    }
+
 }
